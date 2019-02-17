dc86317aa5380f02a2a7bb9a1a112e0bb80ee77b
Manos <manoc.bo@gmail.com>
2019-02-17 11:40:14 +0200

    Add User repository
    A

---
+++ SampleWebApi/Data/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using SampleWebApi.Models;
+
+namespace SampleWebApi.Data
+{
+    public interface IUserRepository
+    {
+        Task CreateUserAsync(User user);
+        Task<User> GetUserByEmailAsync(string email);
+    }
+}
---
+++ SampleWebApi/Data/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SampleWebApi.Models;
+using System.Threading.Tasks;
+
+namespace SampleWebApi.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public UserRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _dataContext.AddAsync(user);
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public Task<User> GetUserByEmailAsync(string email)
+        {
+            return _dataContext.Users.FirstOrDefaultAsync(x => x.Email == email);
+        }
+    }
+}
