ec0240d1778757bb956d525c11e182ce707b134d
Manos <manoc.bo@gmail.com>
2019-02-17 13:03:37 +0200

    Add Users controller

---
+++ SampleWebApi/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SampleWebApi.Services;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
