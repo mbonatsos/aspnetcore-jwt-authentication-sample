8270dede6ef1ae916f8314ebcf06ec11bd04c081
Manos <manoc.bo@gmail.com>
2019-02-17 11:18:06 +0200

    Add User model

---
+++ SampleWebApi/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleWebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+    }
+}
