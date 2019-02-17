b86c834c861e8341d39381f5551884a3272e1b93
Manos <manoc.bo@gmail.com>
2019-02-17 13:07:07 +0200

    Add Dtos for login and registration

---
+++ SampleWebApi/Dtos/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SampleWebApi.Dtos
+{
+    public class UserRegisterDto
+    {
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
