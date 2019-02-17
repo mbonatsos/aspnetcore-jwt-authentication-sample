8ce63789007e6fbb4c5373d52df3739cfe42252b
Manos <manoc.bo@gmail.com>
2019-02-17 12:53:23 +0200

    Add Register method in interface

--- SampleWebApi/Services/IUserService.cs
+++ SampleWebApi/Services/IUserService.cs
@@ -6,5 +6,6 @@ namespace SampleWebApi.Services
     public interface IUserService
     {
         Task<string> Login(User user, string password);
+        Task<User> Register(User user, string password);
     }
 }
